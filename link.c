75e3cedb326113886506220b2fc8a9e25187e39b
Jeff Garzik <jeff@garzik.org>
2006-11-10 11:03:39 -0500

    Update copyright for 2006.

--- link.c
+++ link.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 Jeff Garzik <jgarzik@pobox.com>
+ * Copyright 2004-2006 Jeff Garzik <jgarzik@pobox.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
